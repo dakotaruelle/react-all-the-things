785a52987fbf5de089eaca3499016e43b11ee460
Dakota Ruelle <17259752+dakotaruelle@users.noreply.github.com>
2026-03-30 11:38:45 -0500

    init react-pdf app

---
+++ packages/react-pdf/src/main.tsx
@@ -0,0 +1,12 @@
+import { PDFViewer } from '@react-pdf/renderer';
+import { MyDocument } from './App';
+import { createRoot } from 'react-dom/client';
+
+const App = () => (
+  <PDFViewer style={{ width: '100vw', height: '100vh' }}>
+    <MyDocument />
+  </PDFViewer>
+);
+
+const root = createRoot(document.getElementById('root')!);
+root.render(<App />, );
